733bcc5ea7a7f1e2c2d417da2d09ebb3c486b17a
VaughnCarmeloValenzuela <carmelovaughnvalenzuela@gmail.com>
2024-08-28 12:26:08 +0800

    Add files via upload

---
+++ Exercise Set 02/bitwise1.py
@@ -0,0 +1,24 @@
+x = 12
+y = 25
+outputOr = x | y  
+print (outputOr)
+
+x = 21
+y = 27
+outputAnd = x & y
+print (outputAnd)
+
+x = 8 
+y = 7 
+outputXor = x ^ y
+print (outputXor)
+
+x = 13 
+y = 7 
+outputAnd1 = x & y  
+print (outputAnd1)
+
+x = 13
+y = 7 
+outputOr1 = x | y  
+print (outputOr1)
---
+++ Exercise Set 02/typecasting.py
@@ -0,0 +1,5 @@
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+converted_integer = float(int_input)
+difference = converted_integer - float_input
+print(f"Difference: {difference:.2f}")
